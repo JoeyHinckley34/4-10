eb054de00a3abd3e4c740b717be960dfb6f13bd5
JoeyHinckley34@gmail.com <JoeyHinckley34@gmail.com>
2024-01-31 15:45:16 -0500

    equation class stat

--- 4=10.py
+++ 4=10.py
@@ -1,6 +1,6 @@
 #problem: give n numbers, what combination of + - * / and () makes x
 
-from itertools import permutations, product, zip_longest, combinations
+from itertools import permutations, product, zip_longest
 from pars import genPars
 
 def genAll(nums):
@@ -34,37 +34,51 @@ def genAll(nums):
         for p in parList:
             x = f[:]
             if len(p) != 2:
-                print("ERROR incorrect number of parentheses")
+                raise Exception("ERROR incorrect number of parentheses")
             x.insert(p[0],'(')
             x.insert(p[1],')')
             finalP.append(x)
-
-    # for f in finalP:
-    #     print(f)
 
     #compress the list of lists into a list of strings
     final = [''.join(str(element) for element in f) for f in finalP]
 
     return final
 
+
 def calculate(final,x):
     results = {}
     for f in final:
         try:
             results[f] = eval(f)
+            if results[f] == x:
+                print(results[f])
+                break
         except ZeroDivisionError:
             continue
         
+    #TO DO 
+    #Prune equal solutions
+    
     for key,val in results.items():
         if val == 10:
             print(key,val)
+            break
 
-
+    print("No Solution")
+        
 def main():
-    nums = [7,2,9,2]
 
-    final = genAll(nums)    
-    calculate(final,10)
+    allnums = [ [i,j,k,l] for i in range(10) for j in range(10) for k in range(10) for l in range(10)]
+
+    for a in allnums:
+        print(a)
+        final = genAll(a)    
+        calculate(final,10)
+
+    #nums = [9,8,7,6]
+
+    #final = genAll(nums)    
+    #calculate(final,10)
 
 if __name__ == "__main__":
     main()
---
+++ equations.py
@@ -0,0 +1,25 @@
+from itertools import zip_longest
+
+class equation:
+    def __init__(self,strR):
+        self.strRep = strR
+        try:
+            self.evaluation = eval(self.strRep)
+        except ZeroDivisionError:
+            self.evaluation = 'NaN'
+
+    def __str__(self):
+        return self.strRep + " = " + str(self.evaluation)
+
+
+def test():
+
+    a = equation('1+2+3+4')
+
+
+
+
+    print(a)
+
+
+test()
